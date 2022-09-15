aeacb26480d709e9a836066d383497b16c4aea5d
wcy <123@wcy>
2022-09-15 21:53:05 +0800

    解决Search组件自动填充问题

--- neteasemusic_web/src/views/search/searchComponent.vue
+++ neteasemusic_web/src/views/search/searchComponent.vue
@@ -2,6 +2,8 @@
    <div class='searchComponet'>
     <div class="search_con">
        <el-input placeholder="搜索歌手/歌名/歌词" v-model="keyword" class="searchinput" clearable @change="onSearch"></el-input>
+       //解决密码自动填充的问题
+       <el-input type="text" style="position: fixed; bottom: -99999px;"/>
        <el-button color="red"  class="searchbtn" @click="onSearch"  >搜索</el-button>
     </div> 
    </div> 
